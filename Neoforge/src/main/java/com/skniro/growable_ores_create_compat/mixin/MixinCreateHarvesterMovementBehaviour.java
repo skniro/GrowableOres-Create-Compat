19b1fadc267d2a7b6cec34215356448c69953aee
skniro <46392259+skniro@users.noreply.github.com>
2025-08-05 20:39:00 +0800

    Use Mixin make growable ores compatible with create machine.

---
+++ Neoforge/src/main/java/com/skniro/growable_ores_create_compat/mixin/MixinCreateHarvesterMovementBehaviour.java
@@ -0,0 +1,39 @@
+package com.skniro.growable_ores_create_compat.mixin;
+
+
+import com.simibubi.create.AllTags;
+import com.simibubi.create.content.contraptions.actors.harvester.HarvesterMovementBehaviour;
+import com.skniro.growableores.block.init.GrowableOreCaneBlock;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.GrowingPlantBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(HarvesterMovementBehaviour.class)
+public class MixinCreateHarvesterMovementBehaviour {
+
+    @Inject(method = "isValidOther", at = @At("RETURN"), cancellable = true)
+    private static void isValidOther(Level world, BlockPos pos, BlockState state, CallbackInfoReturnable<Boolean> cir) {
+        Block block = state.getBlock();
+        if(block instanceof GrowableOreCaneBlock){
+            cir.setReturnValue(true);
+        }
+    }
+
+    @Inject(method = "cutCrop", at = @At("HEAD"), cancellable = true)
+    private static void cutCrop(Level world, BlockPos pos, BlockState state, CallbackInfoReturnable<BlockState > cir) {
+        Block block = state.getBlock();
+        if (block instanceof GrowableOreCaneBlock) {
+            if (!state.getFluidState().isEmpty()) {
+                cir.setReturnValue(Blocks.AIR.defaultBlockState());
+            }
+            cir.setReturnValue(state.getFluidState().createLegacyBlock());
+        }
+    }
+}
